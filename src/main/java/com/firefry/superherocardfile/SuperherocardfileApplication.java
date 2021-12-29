36185a790b6ac4446f7cdc14268171f3e54c74ac
С Игорь <igafry@gmail.com>
2021-12-29 23:23:06 +0700

    init

---
+++ src/main/java/com/firefry/superherocardfile/SuperherocardfileApplication.java
@@ -0,0 +1,13 @@
+package com.firefry.superherocardfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperherocardfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperherocardfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firefry/superherocardfile/SuperherocardfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firefry.superherocardfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperherocardfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
